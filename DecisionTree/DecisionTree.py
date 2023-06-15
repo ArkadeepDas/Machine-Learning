4f04e392829eb5a91be924ed957a81f1b1fd7f54
Arkadeep Das <arkadeep.das8@gmail.com>
2023-06-15 19:24:16 +0530

    Load Dataset

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -3,3 +3,8 @@ from sklearn.datasets import load_breast_cancer
 
 # Let's load the dataset
 dataset = load_breast_cancer()
+# Convert dataset to pandas dataframe
+dataset = pd.DataFrame(data = dataset['data'], columns = dataset['feature_names'])
+
+# Let's check the data
+print(dataset.head())
